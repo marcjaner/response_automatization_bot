f8c3726d072ec8347c24c097b716c9755601aa16
Marcufli <marcjanerferrer@gmail.com>
2022-06-29 14:17:05 +0200

    Update outlook.py

--- outlook.py
+++ outlook.py
@@ -2,9 +2,37 @@ from typing_extensions import TypeAlias
 from dataclasses import dataclass
 import win32com.client
 
-
+# --------------------------------------------------------------------------- #
+#                              GLOBAL VARIABLES                               #
+# --------------------------------------------------------------------------- #
 outlook = win32com.client.Dispatch('outlook.application')
 mapi = outlook.GetNamespace("MAPI")
 
-for account in mapi.Accounts:
-	print(account.DeliveryStore.DisplayName)
+# --------------------------------------------------------------------------- #
+#                               VPT VARIABLES                                 #
+# --------------------------------------------------------------------------- #
+vpt = mapi.Folders("contact@vptmallorca.com")
+vpt_inbox = vpt.Folders(1)
+vpt_messages = vpt_inbox.Items
+
+# --------------------------------------------------------------------------- #
+#                             JANERBUS VARIABLES                              #
+# --------------------------------------------------------------------------- #
+# jb = mapi.Folders("bus@janer-bus.com")
+# jb_inbox = jb.Folders(1)
+# jb_messages = jb_inbox.Items
+
+
+# --------------------------------------------------------------------------- #
+#                               GLOBAL MODULES                                #
+# --------------------------------------------------------------------------- #
+
+
+
+# --------------------------------------------------------------------------- #
+#                                VPT MODULES                                  #
+# --------------------------------------------------------------------------- #
+
+
+
+# ----
