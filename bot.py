e184e581ea0f7da6ffca156bc711af55c38ea658
Marcufli <marcjanerferrer@gmail.com>
2022-07-01 18:47:03 +0200

    Minor changes

--- bot.py
+++ bot.py
@@ -29,39 +29,50 @@ def start(update, context):
 
     message: str = "Hola %s, \nAquests són es missatges pendents per contestar. Per qualsevol dubte fés servir sa comanda /help" % (name)
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    get()
+    get(update, context)
 
 def get_bookings(update, context):
-    otl.vpt_summarize_bookings_eng()
-    otl.vpt_summarize_bookings_de()
+    try:
+        otl.vpt_summarize_bookings_eng()
+        otl.vpt_summarize_bookings_de()
 
-    message : str = "Reserves: \n"
+        message : str = "Reserves: \n"
 
-    for i in range(0, len(vpt_bookings)):
-        booking = vpt_bookings[i]
+        for i in range(0, len(vpt_bookings)):
+            booking = vpt_bookings[i]
 
-        message = message + "**%d**. %s -> %s \n%s %d pax \n %s %s\n" % (i, booking.pick_up_arrival, booking.destination_arrival, booking.arrival_date, booking.pax, booking.arrival_time, booking.flight_n_arrival)
+            message = message + "**%d**. %s -> %s \n%s %d pax \n %s %s\n" % (i, booking.pick_up_arrival, booking.destination_arrival, booking.arrival_date, booking.pax, booking.arrival_time, booking.flight_n_arrival)
 
-        if booking.pick_up_departure is not None:
-            message = message + "%s -> %s \n %s %s %s\n" % (booking.pick_up_departure, booking.destination_departure, booking.departure_date, booking.departure_time, booking.flight_n_departure)
-        message = message + "\n"
+            if booking.pick_up_departure is not None:
+                message = message + "%s -> %s \n %s %s %s\n" % (booking.pick_up_departure, booking.destination_departure, booking.departure_date, booking.departure_time, booking.flight_n_departure)
+            message = message + "\n"
 
-    context.user_data['index'] = len(vpt_bookings - 1)
+        context.user_data['index'] = len(vpt_bookings - 1)
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text = message)
+        context.bot.send_message(chat_id=update.effective_chat.id, text = message)
+    except Exception as e:
+        print(e)
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hi ha hagut algun error")
+
 
 def get_quotes(update, context):
-    otl.vpt_summarize_quotes_eng()
-    otl.vpt_summarize_quotes_de()
+    try:
+        otl.vpt_summarize_quotes_eng()
+        otl.vpt_summarize_quotes_de()
 
-    message : str = "Pressuposts: /n"
+        message : str = "Pressuposts: /n"
 
-    for i in range(context.user_data['index'], context.user_data['index'] + len(vpt_quotes)):
-        quote = vpt_quotes[i]
+        for i in range(context.user_data['index'], context.user_data['index'] + len(vpt_quotes)):
+            quote = vpt_quotes[i]
 
-        message = message + "%d. %s %d pax \n\N" % (i, quote.destination, quote.pax)
+            message = message + "%d. %s %d pax \n\n" % (i, quote.destination, quote.pax)
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text = message)
+        context.bot.send_message(chat_id=update.effective_chat.id, text = message)
+    except Exception as e:
+        print(e)
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hi ha hagut algun problema")
 
 
 def get(update, context):
@@ -72,8 +83,8 @@ def get(update, context):
             context.bot.send_message(chat_id=update.effective_chat.id, text = "Sembla que no hi ha cap missatge pendent ")
         else:
 
-            get_bookings()
-            get_quotes()
+            get_bookings(update, context)
+            get_quotes(uptadte, context)
 
 
     except Exception as e:
@@ -81,13 +92,19 @@ def get(update, context):
 
         context.bot.send_message(chat_id=update.effective_chat.id, text = "Sembla que hi ha hagut algún problema, torna-ho a intentar o contacta amb s'informàtic")
 
+def help(update, context):
+    message = "Hola, això és una guia per utilitzar "
+
 
 # --------------------------------------------------------------------------- #
 #                              COMMAND HANDLERS                               #
 # --------------------------------------------------------------------------- #
 
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('get', get))
+dispatcher.add_handler(CommandHandler('getb', get_bookings))
+dispatcher.add_handler(CommandHandler('getq', get_quotes))
 
 updater.start_polling()
 updater.idle()
